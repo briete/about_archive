99fcd0952ad4ef6911f07d0a8f55b5898674aa2a
Sato Naoya <sato.naoya@classmethod.jp>
2023-02-05 00:35:10 +0900

    add:initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [image()]
+});
