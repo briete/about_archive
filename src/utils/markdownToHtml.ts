4e9ec4890b6c11475d4dc8ba9d5d7edf566969fb
Sato Naoya <sato.naoya@classmethod.jp>
2023-02-06 14:23:39 +0900

    なぜかzenn-mark-down-htmlがビルド時のみ not a function で落ちるためラップした

---
+++ src/utils/markdownToHtml.ts
@@ -0,0 +1,13 @@
+import markdownToHtml from "zenn-markdown-html";
+
+let __markdownToHtml: Function;
+if (typeof markdownToHtml == "function") {
+  __markdownToHtml = markdownToHtml;
+} else if ((markdownToHtml as any).hasOwnProperty("default")) {
+  __markdownToHtml = (markdownToHtml as any).default;
+} else {
+  console.error("markdownToHtml is not a support");
+  throw new Error();
+}
+
+export const zennMarkDownToHtml = __markdownToHtml;
